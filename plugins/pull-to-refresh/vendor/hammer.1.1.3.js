9820670d2bf3c92720e943da0658e252737d74a9
brunoziie <eu@brunoziie.com>
2015-02-19 12:50:53 -0300

    remove AMD definition

--- plugins/pull-to-refresh/vendor/hammer.1.1.3.js
+++ plugins/pull-to-refresh/vendor/hammer.1.1.3.js
@@ -2147,17 +2147,6 @@ Hammer.gestures.Touch = {
  * @module hammer
  */
 
-// AMD export
-if(typeof define == 'function' && define.amd) {
-    define(function() {
-        return Hammer;
-    });
-// commonjs export
-} else if(typeof module !== 'undefined' && module.exports) {
-    module.exports = Hammer;
-// browser export
-} else {
-    window.Hammer = Hammer;
-}
+window.Hammer = Hammer;
 
 })(window);
